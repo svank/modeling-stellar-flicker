3e598efa5459fa1351c0c41c872cb0b7ce1674b7
Sam Van Kooten <vankooten.sam@gmail.com>
2019-04-05 15:03:37 -0600

    Some updates diving back into this

--- base.py
+++ base.py
@@ -12,25 +12,26 @@ T_sun = 5777 # K
 logg_sun = 4.438 # log-cgs units, I suppose
 ν_sun = 3.106e-3 # Hz
 
+
 def merge_catalog():
     huber_raw = np.loadtxt("Huber_2014.txt", skiprows=49, usecols=range(19))
-
+    
     huber = dict()
     for i in range(huber_raw.shape[0]):
         row = huber_raw[i]
         huber[int(row[0])] = row
-
+    
     cranmer_raw = np.loadtxt("Cranmer_2014.txt", skiprows=30)
-
+    
     cranmer = dict()
     for i in range(cranmer_raw.shape[0]):
         row = cranmer_raw[i]
         cranmer[int(row[0])] = row
-
+    
     bastien_raw = np.loadtxt("Bastien_2016.txt", skiprows=27, unpack=True)
-
+    
     (KIC, kepmag, F8logg, E_F8logg, e_F8logg, Range, RMS, Teff) = bastien_raw
-
+    
     catalog=np.zeros(len(KIC), dtype=[
                    ('KIC', 'int32'),
                    ('kepmag', 'float64'),
@@ -68,27 +69,7 @@ def merge_catalog():
     catalog['Range'] = Range
     catalog['RMS'] = RMS
     catalog['Teff'] = Teff
-
-    # Values that will be matched from Huber data
-    #TeffH   = np.zeros_like(F8logg)
-    #E_TeffH = np.zeros_like(F8logg)
-    #e_TeffH = np.zeros_like(F8logg)
-    #loggH   = np.zeros_like(F8logg)
-    #E_loggH = np.zeros_like(F8logg)
-    #e_loggH = np.zeros_like(F8logg)
-    #M       = np.zeros_like(F8logg)
-    #E_M     = np.zeros_like(F8logg)
-    #e_M     = np.zeros_like(F8logg)
-
-    #TeffC   = np.zeros_like(F8logg)
-    #loggC   = np.zeros_like(F8logg)
-    #MC      = np.zeros_like(F8logg)
-    #tau_effC= np.zeros_like(F8logg)
-    #MaC     = np.zeros_like(F8logg)
-    #σC      = np.zeros_like(F8logg)
-    #F8_modC = np.zeros_like(F8logg)
-    #F8_obsC = np.zeros_like(F8logg)
-
+    
     for i, kic in enumerate(KIC):
         h_data = huber[kic]
         catalog['TeffH'][i]   = h_data[1]
@@ -97,10 +78,10 @@ def merge_catalog():
         catalog['loggH'][i]   = h_data[4]
         catalog['E_loggH'][i] = h_data[5]
         catalog['e_loggH'][i] = h_data[6]
-        catalog['MH'][i]       = h_data[13]
-        catalog['E_MH'][i]     = h_data[14]
-        catalog['e_MH'][i]     = h_data[15]
-
+        catalog['MH'][i]      = h_data[13]
+        catalog['E_MH'][i]    = h_data[14]
+        catalog['e_MH'][i]    = h_data[15]
+        
         if kic in cranmer:
             catalog['has_C'][i] = 1
             c_data = cranmer[kic]
@@ -112,16 +93,12 @@ def merge_catalog():
             catalog['sigmaC'][i] = c_data[7]
             catalog['F8_modC'][i] = c_data[8]
             catalog['F8_obsC'][i] = c_data[9]
-    #pickle.dump(catalog, open('merged_catalog.pkl', 'wb'))
     np.save('merged_catalog.npy', catalog)
 
+
 def load_catalog(fname='merged_catalog.npy'):
-    #return pickle.load(open(fname))
     return np.load(fname)
 
-#merge_catalog()
-
-catalog = load_catalog()
 
 def F8_from_logg(logg_array):
     """Calculates F8 flicker values from log(g)
@@ -137,19 +114,20 @@ def F8_from_logg(logg_array):
         b = -1.6838185
         c = -3.5002686
         d =  1.3724221 - logg
-
+        
         # Numpy solves for roots generally, computing "the eigenvalues of
         # the companion matrix"
         roots = np.roots([a, b, c, d])
         # Select real root
         root = roots[np.imag(roots) == 0]
         root = np.real(root)
-
-        if (root.size != 1):
+        
+        if root.size != 1:
             raise RuntimeError("Cubic root is ambiguous")
-
+        
         F8[i] = 10**root
     return F8
+
 
 def calc_σ(Teff, M, logg, S=1, Φ=None):
     """Calculates RMS amplitude σ of photospheric continuum intensity variations
@@ -159,14 +137,17 @@ def calc_σ(Teff, M, logg, S=1, Φ=None):
     Ma = calc_Ma(logg, Teff)
     Ma *= S
     if Φ is None:
+        # TODO: Why is the phi(.26) here? It's not in the paper
         Φ = calc_Φ(Ma) / calc_Φ(.26)
         
         # Clamp Φ at zero
         Φ = 0 * (Φ < 0) + Φ * (Φ >= 0)
+    
+    # Why 1.03 power when Samadi has 1.10? See Samadi Figure B.1
+    return 0.039 * ( (Teff / T_sun) ** (3/4)
+                     * (M_sun * ν_sun / M / ν_max) ** (1/2)
+                     * Φ**2 )**1.03
 
-    return  0.039 * ( (Teff / T_sun) ** (3/4)
-                   * (M_sun * ν_sun / M / ν_max) ** (1/2)
-                   * Φ**2 )**1.03
 
 def calc_ν_max(logg, Teff):
     """Calculates peak p-mode frequency
@@ -174,27 +155,33 @@ def calc_ν_max(logg, Teff):
     """
     return ν_sun * 10**logg / 10**logg_sun * (T_sun / Teff)**0.5
 
+
 def calc_Ma(logg, Teff):
     """Calculates Mach number
     Cranmer 2014's Eqn 3
     """
     return 0.26 * (Teff / T_sun)**2.35 * (10**logg_sun / 10**logg) ** 0.152
 
+
 def calc_Φ(Ma, make_monotonic=True):
     """Calculates the temperature fluctuation amplitude
     Cranmer 2014's Eqn 4
     """
+    
+    # TODO: Check in on these---numbers might be reported wrong in Steve's paper,
+    # but calculations behind his plots should be fine
     A = -.59/.26/.26 # = -8.73
     B = 2.3/.26      # =  8.85
     C = -0.67        # = -0.67
     
     max_val = ( (-B**2 / (4*A)) + C )
     quadratic_val = A * Ma**2 + B * Ma + C
-
+    
     # If Ma is beyond the Ma of the maximal Φ,
     # clamp to the maximal Φ instead
-    return (make_monotonic * Ma > (-B/2/A)) * max_val \
-         + ((not make_monotonic) or Ma <= (-B/2/A)) * quadratic_val
+    return ((make_monotonic * Ma > (-B/2/A)) * max_val
+            + ((not make_monotonic) or Ma <= (-B/2/A)) * quadratic_val)
+
 
 def calc_F8_from_σ(logg, Teff, σ, S=1):
     """Cranmer 2014's Eqn 8"""
@@ -202,76 +189,88 @@ def calc_F8_from_σ(logg, Teff, σ, S=1):
     ν_max = calc_ν_max(logg, Teff)
     Ma = calc_Ma(logg, Teff) * S
     Ma_sun = 0.26 # As given in Cranmer 2014
-
+    
     τ_eff = 300 * (ν_sun * Ma_sun / ν_max / Ma)**0.98
-
+    
     return σ * np.sqrt(1 - 2 / np.pi * np.arctan(4 * τ_eff * ν_8))
+
 
 def calc_F8(logg, Teff, M, S=1, Φ=None):
     σ = calc_σ(Teff, M, logg, S, Φ)
     return calc_F8_from_σ(logg, Teff, σ, S)
 
+
 def calc_cranmer_S(Teff):
     value = 1 / (1 + (Teff - 5400) / 1500)
     return 1 * (Teff <= 5400) + value * (Teff > 5400)
 
-def fit_S(logg_arr, Teff_arr, M_arr, F8obs_arr):
+
+def fit_S(logg_arr, Teff_arr, M_arr, F8obs_arr, max_S=2, too_large_fill=0):
     S = np.zeros_like(logg_arr)
     
-    for i in range(len(S)): 
+    for i in range(len(S)):
         logg = logg_arr[i]
         Teff = Teff_arr[i]
         M = M_arr[i]
         F8obs = F8obs_arr[i]
-
+        
         # The objective function to be minimized
         obj = lambda S: calc_F8(logg, Teff, M, S) - F8obs
-
+        
         try:
             S[i] = scipy.optimize.newton(obj, x0=calc_cranmer_S(Teff))
         except:
             S[i] = float('nan')
             #s0 = calc_cranmer_S(Teff)
             #print(i, s0, calc_F8(logg, Teff, M, s0), calc_F8(logg, Teff, M, 1), F8obs)
-
-    # Set values >= 2 to 0
-    S *= S < 2
+    
+    # Set values >= max to fill value
+    S[S > max_S] = too_large_fill
     return S
+
 
 def fit_Φ(logg_arr, Teff_arr, M_arr, F8obs_arr):
     Φ = np.zeros_like(logg_arr)
-
+    
     for i in range(len(Φ)):
         logg = logg_arr[i]
         Teff = Teff_arr[i]
         M = M_arr[i]
         F8obs = F8obs_arr[i]
-
+        
         # The objective function to be minimized
         obj = lambda Φ: calc_F8(logg, Teff, M, Φ=Φ) - F8obs
-
+        
         try:
             Φ[i] = scipy.optimize.newton(obj, x0=3)
         except:
             Φ[i] = float('nan')
-
+    
     return Φ
 
 
-def plot_outline():
+def plot_outline(newer_version=True):
     x = np.linspace(.015, .34, 100)
-
-    poly_fit = lambda x: 1.3724221 - 3.5002686*np.log10(x) - 1.6838185*np.log10(x)**2 - 0.37909094*np.log10(x)**3
+    
+    if newer_version:
+        poly_fit = lambda x: 1.3724221 - 3.5002686*np.log10(x) - 1.6838185*np.log10(x)**2 - 0.37909094*np.log10(x)**3
+    else:
+        poly_fit = lambda x: 1.15136 - 3.59637*np.log10(x) - 1.40002*np.log10(x)**2 - 0.22993*np.log10(x)**3
+        
     plt.plot(x, poly_fit(x) + 0.2, color='black')
     plt.plot(x, poly_fit(x) - 0.2, color='black')
-
+    
     plt.plot([.015, .015], [poly_fit(.015)+.2, poly_fit(.015)-.2], color='black')
     plt.plot([.34, .34], [poly_fit(.34)+.2, poly_fit(.34)-.2], color='black')
-
+    
     plt.gca().invert_yaxis()
 
-catalog['F8'] = F8_from_logg(catalog['F8logg'])
 
+#merge_catalog()
+
+catalog = load_catalog()
+
+catalog['F8'] = F8_from_logg(catalog['F8logg'])
 
 #F8 = F8_from_logg(F8logg)
 
